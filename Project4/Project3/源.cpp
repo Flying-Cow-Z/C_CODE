aa366927b13f3cecbffe60fbc71e6bc05999b4c9
Flying-Cow-Z <2511767449@qq.com>
2020-09-04 12:29:47 +0800

    入门代码1

---
+++ Project4/Project3/源.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#pragma warning(disable:4996)
+int main()
+{
+	char arr[] = { 'b', 'i', 't' };
+	printf("%d\n", strlen(arr));
+	system("pause");
+	return 0;
+}
