775936567e06fa89f5b6d81b167436c991b8a219
Flying-Cow-Z <2511767449@qq.com>
2020-09-26 23:38:36 +0800

    C第一条代码

---
+++ Project2/Project2/源.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<windows.h>
+
+//下面代码，打印结果是什么？为什么？（突出'\0'的重要性）
+int main()
+{
+	char arr1[] = "bit";
+	char arr2[] = { 'b', 'i', 't' };
+	char arr3[] = { 'b', 'i', 't', '\0' };
+	printf("%s\n", arr1);
+	printf("%s\n", arr2);
+	printf("%s\n", arr3);
+
+	system("pause");
+	return 0;
+}
