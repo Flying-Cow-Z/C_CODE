83dda9c7730e737837a5681c1634f6a2c249a86d
Flying-Cow-Z <2511767449@qq.com>
2020-10-06 18:45:38 +0800

    example

---
+++ Project9/Project9/源.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<windows.h>
+
+int main()
+{
+	int a = 1;
+
+
+	switch (a)
+	{
+	case 1:
+			
+			
+		case 2:
+			
+		case 3:
+		
+		case 4:
+		
+		case 5:
+			printf("weekday\n");
+			break;
+		case 6:
+			
+		case 7:
+			printf("weekend\n");
+			break;
+	}
+	system("pause");
+	return 0;
+}
