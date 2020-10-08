29d12938a60c5d146940b1efebe7f12ef4fed87c
Flying-Cow-Z <2511767449@qq.com>
2020-10-09 00:48:31 +0800

    example

---
+++ Project13/Project13/源.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main()
+{
+
+	int i = 0;
+	int k = 0;
+	for (i = 0, k = 0; k = 0; i++, k++)
+	{
+		k++;
+		printf("ok\n");
+	}
+	system("pause");
+	return 0;
+}
