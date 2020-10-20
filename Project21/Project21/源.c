dd85344cd0b2743001b94bc3dced1e2a294e52e0
Flying-Cow-Z <2511767449@qq.com>
2020-10-20 23:10:38 +0800

    传地址交换函数

---
+++ Project21/Project21/源.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<windows.h>
+
+int swap(int *x, int *y)
+{
+	printf("swap:交换之前：%d %d\n", *x, *y);
+	int temp = 0;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+	printf("swap:交换之前：%d %d\n", *x, *y);
+}
+
+int main()
+{
+	int a = 20;
+	int b = 30;
+	printf("main:交换之前：%d %d\n", a, b);
+	swap(&a, &b);
+	printf("main:交换之后：%d %d\n", a, b);
+	system("pause");
+	return 0;
+}
